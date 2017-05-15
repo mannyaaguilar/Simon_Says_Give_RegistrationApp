5eee49cbd621378950d544ec4b9acb3907ca2bdc
Lisa Schoofs <lisaschoofs@gmail.com>
2017-05-15 12:17:21 -0500

    adds timer functionality for page redirect

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -55,12 +55,32 @@ console.log("adminController Loaded");
 $scope.redirect = UserService.redirect;
 }]);
 
-myApp.controller('ConfirmationController', ['$scope', '$http', '$location', 'UserService', function($scope, $http, $location, UserService) {
+myApp.controller('ConfirmationController', ['$scope', '$http', '$location', '$interval', 'UserService', function($scope, $http, $location, $interval, UserService) {
 
   $scope.userObject = UserService.userObject;
   $scope.logout = UserService.logout;
   $scope.redirect = UserService.redirect;
 
+  // function that changes view to check-in/check-out after 15 seconds
+  $scope.enabled = true;
+  $scope.timer = 0;
+
+  $scope.timeOut = function() {
+    var interval = $interval(function(){
+      if ($scope.timer < 10){
+        $scope.timer += 1;
+        console.log('+ 1 second!');
+        }
+      else {
+      // $location.path('/checkInOut');
+      $location.path('/user');
+      $interval.cancel(interval);
+      }
+    }, 1000);
+  }; //end timeout
+
+  // timeOut function runs on view load
+  $scope.timeOut();
 }]);
 
 myApp.controller('LoginController', ['$scope', '$http', '$location', function($scope, $http, $location) {
