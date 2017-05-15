427226b79e332acbfbe8742e3e5aec6a3d87f7f9
Lisa Schoofs <lisaschoofs@gmail.com>
2017-05-15 12:25:39 -0500

    adds change view functionality to done button

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -61,26 +61,32 @@ myApp.controller('ConfirmationController', ['$scope', '$http', '$location', '$in
   $scope.logout = UserService.logout;
   $scope.redirect = UserService.redirect;
 
-  // function that changes view to check-in/check-out after 15 seconds
   $scope.enabled = true;
   $scope.timer = 0;
+  var timeOutLength = 3;
 
+  // function that changes view to check-in/check-out after x amount of seconds
   $scope.timeOut = function() {
     var interval = $interval(function(){
-      if ($scope.timer < 10){
+      if ($scope.timer < timeOutLength){
         $scope.timer += 1;
-        console.log('+ 1 second!');
         }
       else {
-      // $location.path('/checkInOut');
-      $location.path('/user');
-      $interval.cancel(interval);
+        $scope.changeView();
+        $interval.cancel(interval);
       }
     }, 1000);
   }; //end timeout
 
-  // timeOut function runs on view load
+  //changes view to checkInOut page
+  $scope.changeView = function() {
+    // $location.path('/checkInOut');
+    $location.path('/user');
+  };
+
+  // timeOut function runs on view load - redirects page in 10 seconds
   $scope.timeOut();
+
 }]);
 
 myApp.controller('LoginController', ['$scope', '$http', '$location', function($scope, $http, $location) {
