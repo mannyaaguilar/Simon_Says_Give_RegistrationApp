f9dee56575953ad0d2eea97987b665105b283151
Lisa Schoofs <lisaschoofs@gmail.com>
2017-05-16 14:21:25 -0500

    test list of volunteers appending to DOM with ng-repeat

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -108,14 +108,17 @@ myApp.controller('checkInOutController', ['$scope', '$location', function($scope
 
 myApp.controller('CheckoutController', ['$scope', '$location', function($scope, $location) {
 
-$scope.volunteerObject = {
-    firstName: '',
-    lastName: '',
-    email: '',
-};
+//object for input items to bind to
+$scope.volunteerObject = {};
 
+//variable to inform the ng-show on the search results div
 $scope.success = false;
 
+//Array to assign search results to - has dummy info for now
+$scope.volunteerList = [1,2,3,4,5];
+
+//Connected to Search button - take inputs and check for records in database,
+// append results to DOM
 $scope.search = function() {
   console.log('volunteerObject: ', $scope.volunteerObject);
   //NEED TO ADD: GET to collect matching records by email and/or name
@@ -125,7 +128,6 @@ $scope.search = function() {
 $scope.checkout = function() {
   //NEED TO ADD: PUT ROUTE to add checkout time to chosen volunteer hours record
   console.log('Logging checkout time on click: ', new Date());
-
   //changes view to confirmation page:
   $scope.changeView();
 };
