ebae9d07a8f1f286c341f717f0695380f90a78b0
Erin Kinnen <erin.kinnen@outlook.com>
2017-05-17 10:04:04 -0500

    removed comments and corrected html

--- client/scripts/controllers/volunteerController.js
+++ client/scripts/controllers/volunteerController.js
@@ -13,31 +13,5 @@ var volunteer = {
   dob: ''
 };
 
-// $scope.birthDate = new Date();
-//
-//   // this.minDate = new Date(
-//   //   this.birthDate.getFullYear(),
-//   //   this.birthDate.getMonth() - 2,
-//   //   this.birthDate.getDate()
-//   // );
-//   //
-//   this.maxDate = new Date(
-//     this.birthDate.getFullYear() - 8,
-//     this.birthDate.getMonth() ,
-//     this.birthDate.getDate()
-//   );
 
-//if under_18 = true, redirect to childWaiver, if false, redirect to adultWaiver
-//
-// $scope.volunteerCheckIn = function(volunteer){
-//   console.log("volunteerCheckIn function accessed");
-//   if(volunteer.under_18 === true){
-//     console.log(volunteer.first_name);
-//     $location.path('/waiver-youth');
-//   } else if (volunteer.has_signed_waiver === true) {
-//     $location.path('/waiver-photo');
-//   } else {
-//     $location.path('/waiver-adult');
-//   }
-// };
 }]);
