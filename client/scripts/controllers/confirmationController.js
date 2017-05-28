6ba0e9a40a6ca370b568677d4fca8a999fcdaf88
Lisa Schoofs <lisaschoofs@gmail.com>
2017-05-28 15:20:57 -0500

    $timeout functioning, times out at 7 seconds

--- client/scripts/controllers/confirmationController.js
+++ client/scripts/controllers/confirmationController.js
@@ -1,20 +1,12 @@
-myApp.controller('ConfirmationController', ['$scope', '$http', '$location', '$interval', 'UserService', '$timeout', function($scope, $http, $location, $interval, UserService, timeout) {
+myApp.controller('ConfirmationController', ['$scope', '$http', '$location', '$interval', 'UserService', '$timeout', function($scope, $http, $location, $interval, UserService, $timeout) {
 
   $scope.userObject = UserService.userObject;
   $scope.logout = UserService.logout;
   $scope.redirect = UserService.redirect;
 
-  $scope.$timeout( function(){
+  //On page timeout, view changes to /checkInOut. Timeout measured in milliseconds
+  $scope.timeOut = $timeout( function(){
     $location.path('/checkInOut');
-  }, 5000 );
-
-
-  //changes view to checkInOut page
-  $scope.changeView = function() {
-    $location.path('/checkInOut');
-  };
-
-  // timeOut function runs when confirmation view is loaded
-  // $scope.timeOut();
+  }, 7000 );
 
 }]);
