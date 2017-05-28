5dde71ef8c5fbaf04b5e017d0e01d19bf2328e08
Lisa Schoofs <lisaschoofs@gmail.com>
2017-05-28 09:04:29 -0500

    removes interval, start of $timeout

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -354,37 +354,24 @@ $scope.changeView = function() {
 
 }]);
 
-myApp.controller('ConfirmationController', ['$scope', '$http', '$location', '$interval', 'UserService', function($scope, $http, $location, $interval, UserService) {
+myApp.controller('ConfirmationController', ['$scope', '$http', '$location', '$interval', 'UserService', '$timeout', function($scope, $http, $location, $interval, UserService, timeout) {
 
   $scope.userObject = UserService.userObject;
   $scope.logout = UserService.logout;
   $scope.redirect = UserService.redirect;
 
-  $scope.timer = 0; // timer starts at 0 seconds
-  var timeOutLength = 5; //page will change view after x seconds
+  $scope.$timeout( function(){
+    $location.path('/checkInOut');
+  }, 5000 );
 
-  // function that changes view to check-in/check-out view after x seconds
-  $scope.timeOut = function() {
-    var interval = $interval(function(){
-      if ($scope.timer < timeOutLength){
-        $scope.timer += 1;
-        }
-      else {
-        $scope.changeView();
-        $interval.cancel(interval);
-      }
-    }, 1000);
-  }; //end timeout
 
   //changes view to checkInOut page
   $scope.changeView = function() {
     $location.path('/checkInOut');
-    //user path for testing:
-    // $location.path('/user');
   };
 
   // timeOut function runs when confirmation view is loaded
-  $scope.timeOut();
+  // $scope.timeOut();
 
 }]);
 
