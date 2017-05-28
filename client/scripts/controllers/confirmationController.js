ed3651e5b876fbcb699f77bad44432345a201db8
Lisa Schoofs <lisaschoofs@gmail.com>
2017-05-28 16:07:19 -0500

    timeOut cancels on changeview

--- client/scripts/controllers/confirmationController.js
+++ client/scripts/controllers/confirmationController.js
@@ -4,14 +4,22 @@ myApp.controller('ConfirmationController', ['$scope', '$http', '$location', '$in
   $scope.logout = UserService.logout;
   $scope.redirect = UserService.redirect;
 
-  //On page timeout, view changes to /checkInOut. Timeout measured in milliseconds
-  $scope.timeOut = $timeout( function(){
-    $location.path('/checkInOut');
-  }, 7000 );
+  var timer;
 
-  //changes view to checkInOut page
+  //On page timeout, view changes to /checkInOut. Timeout measured in milliseconds
+  $scope.timeOut = function(){
+    timer = $timeout(function () {
+      $scope.changeView();
+    }, 7000);
+  };
+
+  //timeOut function call on page load
+  $scope.timeOut();
+
+  //changes view to checkInOut page, cancels timout
   $scope.changeView = function() {
     $location.path('/checkInOut');
+    $timeout.cancel(timer);
   };
 
 }]);
