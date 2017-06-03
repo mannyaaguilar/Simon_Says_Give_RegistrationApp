645080342d5275d21893b54aae9e1d745bf127fd
Claudia Calderas <claudia.calderas@gmail.com>
2017-06-03 12:40:42 -0500

    cleaned and commented code

--- client/scripts/controllers/addAdminController.js
+++ client/scripts/controllers/addAdminController.js
@@ -1,8 +1,11 @@
 myApp.controller('AddAdminController', ['$scope', '$http', '$location', 'UserService', 'UtilitiesService',
                 function($scope, $http, $location, UserService, UtilitiesService) {
 
+  // function that uses $location to change to the required view
   $scope.redirect = UserService.redirect;
+  // message string for user's alert
   var message = '';
+  // object with information to send to the server
   $scope.adminUser = {
     username: '',
     password: '',
@@ -15,14 +18,11 @@ myApp.controller('AddAdminController', ['$scope', '$http', '$location', 'UserSer
     if($scope.adminUser.username == '' || $scope.adminUser.password == '' || $scope.adminUser.email == '') {
       UtilitiesService.showAlert('Please enter all the required information.');
     } else {
-      console.log('sending to server...', $scope.adminUser);
       $http.post('/register', $scope.adminUser).then(function(response) {
-        console.log('success');
         message = $scope.adminUser.username + ' has been added as an Admin User.';
         showAlert(message);
       },
       function(response) {
-        console.log('error');
         message = "Error adding admin. Please make sure admin doesn’t already exist."
         showAlert(message);
       });
