b53813617b456117261eff714af7b6d867933122
Claudia Calderas <claudia.calderas@gmail.com>
2017-06-05 10:04:13 -0500

    removed footer from volunteer views and unused files

--- client/scripts/controllers/adminController.js
+++
@@ -1,5 +0,0 @@
-myApp.controller('AdminController', ['$scope', '$http', '$location', 'UserService', function($scope, $http, $location, UserService) {
-console.log("adminController Loaded");
-
-$scope.redirect = UserService.redirect;
-}]);
